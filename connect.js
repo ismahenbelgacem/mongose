acab40ffa7e61d7da36951312ad10867bb63465f
ismahen <mouhamedismahen@gmail.com>
2023-02-10 21:16:17 +0100

    first commit

---
+++ connect.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+
+const connect=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("db connect")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports=connect
